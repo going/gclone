46afdd34da9d4539415983cd3bd466c2df76439c
Tony.Shao <xiocode@gmail.com>
2022-11-25 13:13:13 +0800

    upgrade rclone to v1.60.1

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e CC=gcc -e GOOS=linux -e GOARCH=amd64 golang:alpine go build -tags "full" -ldflags="-s -w" -v
+docker run --platform=linux/amd64 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e CC=gcc -e GOOS=linux -e GOARCH=amd64 golang:alpine go build -tags "full" -ldflags="-s -w" -v
